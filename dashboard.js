dda786006a7d4a4dd622c995a46fc80f4deb7a7c
deepak patel <dp089901@gmail.com>
2024-06-28 19:21:50 +0530

    logout functionality in dashboard.js and dashboard.html

--- dashboard.js
+++ dashboard.js
@@ -1,11 +1,42 @@
+let finalinfo;
 (function(){
    let info =  window.localStorage.getItem("information");
 
-   let finalinfo = JSON.parse(info);
+   finalinfo = JSON.parse(info);
 
    let ui = document.getElementById('logo');
 
   ui.innerHTML =  finalinfo.map(items=>`
     <img style="width:70px;height:70px;border-radius:50%" src="${items.img}">
-    <div>${items.username}</div>`)
+   `)
 })()
+
+let count = 0;
+function showdata(){
+         if(count%2==0){         
+         document.querySelector('#showinfo').innerHTML = finalinfo.map(items=>`
+              <div>
+               <div>${items.username}</div>
+               <div>${items.mobile}</div>
+               <div onclick="logout()">logout</div>
+              </div>
+         `
+        )
+
+        count++
+         }
+         else if(count%2!=0){
+          document.querySelector('#showinfo').innerHTML ="";
+          count++
+         }
+}
+
+function logout(){
+  let out = window.confirm("do you want to logout");
+  if(out){
+    window.localStorage.clear("information")
+  window.open("login.html")   
+  }else{
+
+  }
+}
